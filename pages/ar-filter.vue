4bcb19f408f15cc9c0ac0371525f2ff855f91b17
Liam K <karllauy@gmail.com>
2020-12-29 15:50:42 +0800

    create splash page

---
+++ pages/ar-filter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <face-filter />
+    </div>
+</template>
+
+<script>
+import FaceFilter from '~/components/FaceFilter.vue'
+export default {
+  components: { FaceFilter },
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
