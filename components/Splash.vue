bd5ea4f6baf945a00f43819ea2e0b6e8f3c3b702
Liam K <karllauy@gmail.com>
2020-12-29 15:40:19 +0800

    forget password

--- components/Splash.vue
+++ components/Splash.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="container absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center">
-        <img src="../static/img/bliss.svg" alt="Bliss" class="mb-20">
+        <img src="../static/img/bliss.svg" alt="Bliss" class="mb-16">
         <social-login-btn buttonText="Sign in with Apple" class="mb-8" />
         <social-login-btn buttonText="Sign in with Facebook" class="mb-8"/>
-        <social-login-btn buttonText="Sign in with Phone number" class="mb-8" />
+        <social-login-btn buttonText="Sign in with Phone number" class="mb-10" />
+        <div class="text-white underline karla-font">Trouble Signing in</div>
     </div>
 </template>
 
@@ -28,6 +29,12 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Karla:wght@700&display=swap');
+
+.karla-font{
+    font-family: 'Karla', sans-serif;
+}
+
 .container {
     background-image: url('../static/img/splash-image-purple.svg');
     background-size: 100% 100%;
