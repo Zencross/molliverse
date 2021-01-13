62aef90cc2fcbf80189f92e2cdf1d04e2a395891
Liam K <karllauy@gmail.com>
2021-01-13 16:03:21 +0800

    disable zooming

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     title: 'Bliss',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
