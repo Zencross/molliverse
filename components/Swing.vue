a7bdc26be750acdb4d4c110d8126150bf7ce6bfb
Taral Gurung <txg818@student.bham.ac.uk>
2021-01-27 18:28:02 +0800

    same as last commit, pushing for merging to develop

--- components/Swing.vue
+++ components/Swing.vue
@@ -1,16 +1,5 @@
 <template>
-    <!-- <div> -->
-        <!-- <div class="flex bottom-0 absolute left-0 right-0 pb-5 pl-16 md:ml-24">
-            <a href="#">
-                <img src="../assets/img/remove.png" class="w-2/5" v-on:click="remove">
-            </a>
-            <a href="#">
-                <img src="../assets/img/tick.png" class="w-2/5" v-on:click="swing">
-            </a>
-            <a href="#">
-                <img src="../assets/img/circle.png" class="w-2/5" v-on:click="add">
-            </a>
-        </div> -->
+    <div> 
         <div>
             <vue-swing
                 @throwout="onThrowout"
@@ -23,7 +12,19 @@
                 </div>
             </vue-swing>
         </div>
-    <!-- </div> -->
+
+         <div class="flex bottom-0 absolute left-0 right-0 pb-5 pl-16 md:ml-24">
+            <a href="#">
+                <img src="../assets/img/remove.png" class="w-2/5" v-on:click="remove">
+            </a>
+            <a href="#">
+                <img src="../assets/img/tick.png" class="w-2/5" v-on:click="swing">
+            </a>
+            <a href="#">
+                <img src="../assets/img/circle.png" class="w-2/5" v-on:click="add">
+            </a>
+        </div> 
+    </div>
 </template>
 
 <script>
