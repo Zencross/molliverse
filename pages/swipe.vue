7ebd47b610fd4f7ff8a998e0a3685e22ced8aac7
Liam K <karllauy@gmail.com>
2021-03-09 16:48:03 +0800

    indication bar on carousel

--- pages/swipe.vue
+++ pages/swipe.vue
@@ -8,7 +8,7 @@
       @clickMessager="onClickMessager"
     ></TopBar>
 
-    <div id="swipe" class="relative m-4 bg-gray-200">
+    <div id="swipe" class="relative flex justify-center m-4">
       <!-- Hidden listeners for carousel -->
       <div class="absolute z-30 flex w-full h-full">
         <div
@@ -20,6 +20,19 @@
           id="nextMedia"
           class="z-30 w-1/2 h-full bg-transparent rounded-xl"
           @click="onClickNextMedia"
+        ></div>
+      </div>
+
+      <div class="absolute z-40 flex w-full px-2 mt-4 bg-transparent">
+        <div
+          v-for="media in userProfileMedia"
+          :key="media.id"
+          class="w-full h-1 mx-1 bg-white rounded-full opacity-50"
+          :class="[
+            userProfileMedia.indexOf(media) === currentMediaIndex
+              ? 'opacity-100'
+              : ''
+          ]"
         ></div>
       </div>
 
@@ -85,32 +98,6 @@ export default {
     getMediaSrc() {
       return this.$store.state.userProfileMedia[this.currentMediaIndex].src;
     },
-    // isMediaPhoto() {
-    //   if (
-    //     this.$store.state.userProfileMedia[this.currentMediaIndex] &&
-    //     this.$store.state.userProfileMedia[this.currentMediaIndex].type ===
-    //       "photo"
-    //   ) {
-    //     console.log(
-    //       `media at index ${this.currentMediaIndex} is`,
-    //       this.$store.state.userProfileMedia[this.currentMediaIndex].type
-    //     );
-    //     return true;
-    //   } else return false;
-    // },
-    // isMediaVideo() {
-    //   if (
-    //     this.$store.state.userProfileMedia[this.currentMediaIndex] &&
-    //     this.$store.state.userProfileMedia[this.currentMediaIndex].type ===
-    //       "video"
-    //   ) {
-    //     console.log(
-    //       `media at index ${this.currentMediaIndex} is`,
-    //       this.$store.state.userProfileMedia[this.currentMediaIndex].type
-    //     );
-    //     return true;
-    //   } else return false;
-    // },
     onClickLastMedia() {
       console.log("last Media clicked");
       if (this.currentMediaIndex !== 0) {
