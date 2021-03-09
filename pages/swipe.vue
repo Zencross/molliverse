6b3ee0be099240fcb18f395d25487ede25ad732e
Liam K <karllauy@gmail.com>
2021-03-09 17:52:33 +0800

    simple dragging

--- pages/swipe.vue
+++ pages/swipe.vue
@@ -8,53 +8,62 @@
       @clickMessager="onClickMessager"
     ></TopBar>
 
-    <div id="swipe" class="relative flex justify-center m-4">
-      <!-- Hidden listeners for carousel -->
-      <div class="absolute z-30 flex w-full h-full">
-        <div
-          id="lastMedia"
-          class="z-30 w-1/2 h-full bg-transparent rounded-xl"
-          @click="onClickLastMedia"
-        ></div>
-        <div
-          id="nextMedia"
-          class="z-30 w-1/2 h-full bg-transparent rounded-xl"
-          @click="onClickNextMedia"
-        ></div>
-      </div>
+    <Vue2InteractDraggable
+      @draggedRight="draggedRight"
+      @draggedLeft="draggedLeft"
+      :interact-max-rotation="15"
+      :interact-out-of-sight-x-coordinate="800"
+      :interact-x-threshold="135"
+      :interact-lock-y-axis="true"
+    >
+      <div id="swipe" class="relative flex justify-center m-4">
+        <!-- Hidden listeners for carousel -->
+        <div class="absolute z-30 flex w-full h-full">
+          <div
+            id="lastMedia"
+            class="z-30 w-1/2 h-full bg-transparent rounded-xl"
+            @click="onClickLastMedia"
+          ></div>
+          <div
+            id="nextMedia"
+            class="z-30 w-1/2 h-full bg-transparent rounded-xl"
+            @click="onClickNextMedia"
+          ></div>
+        </div>
 
-      <div class="absolute z-40 flex w-full px-2 mt-4 bg-transparent">
-        <div
-          v-for="media in userProfileMedia"
-          :key="media.id"
-          class="w-full h-1 mx-1 bg-white rounded-full opacity-50"
-          :class="[
-            userProfileMedia.indexOf(media) === currentMediaIndex
-              ? 'opacity-100'
-              : ''
-          ]"
-        ></div>
-      </div>
+        <div class="absolute z-40 flex w-full px-2 mt-4 bg-transparent">
+          <div
+            v-for="media in userProfileMedia"
+            :key="media.id"
+            class="w-full h-1 mx-1 bg-white rounded-full opacity-50"
+            :class="[
+              userProfileMedia.indexOf(media) === currentMediaIndex
+                ? 'opacity-100'
+                : ''
+            ]"
+          ></div>
+        </div>
 
-      <div class="absolute top-0 w-full h-full">
-        <img
-          id="mediaImg"
-          v-if="isMediaPhoto"
-          :src="getMediaSrc()"
-          class="object-cover w-full h-full rounded-xl"
-          alt="photo"
-        />
-        <video
-          id="mediaVideo"
-          v-if="isMediaVideo"
-          :src="getMediaSrc()"
-          autoplay
-          playsinline
-          loop
-          class="object-cover w-full h-full rounded-xl"
-        ></video>
+        <div class="absolute top-0 w-full h-full">
+          <img
+            id="mediaImg"
+            v-if="isMediaPhoto"
+            :src="getMediaSrc()"
+            class="object-cover w-full h-full rounded-xl"
+            alt="photo"
+          />
+          <video
+            id="mediaVideo"
+            v-if="isMediaVideo"
+            :src="getMediaSrc()"
+            autoplay
+            playsinline
+            loop
+            class="object-cover w-full h-full rounded-xl"
+          ></video>
+        </div>
       </div>
-    </div>
+    </Vue2InteractDraggable>
 
     <div
       class="absolute bottom-0 z-50 flex justify-between w-full h-12 px-4 mb-3"
@@ -78,9 +87,10 @@
 
 <script>
 import TopBar from "~/components/TopBar";
+import { Vue2InteractDraggable } from "vue2-interact";
 
 export default {
-  components: { TopBar },
+  components: { TopBar, Vue2InteractDraggable },
   data() {
     return {
       isMediaPhoto: false,
@@ -124,6 +134,12 @@ export default {
         this.isMediaPhoto = false;
         this.isMediaVideo = true;
       }
+    },
+    draggedRight() {
+      console.log("dragged right");
+    },
+    draggedLeft() {
+      console.log("dragged left");
     }
   },
   computed: {
