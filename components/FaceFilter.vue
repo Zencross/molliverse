aae99cfbbddd93e5f8e7c21c01314522913da3f8
Liam K <karllauy@gmail.com>
2021-08-25 13:47:48 +0800

    remove codecs=vp8 from blob formation

--- components/FaceFilter.vue
+++ components/FaceFilter.vue
@@ -184,7 +184,8 @@ export default {
 
       this.mediaRecorder.onstop = async e => {
         console.log("video onStop");
-        var blob = new Blob(chunks, { type: "video/webm;codecs=vp8" });
+        //var blob = new Blob(chunks, { type: "video/webm;codecs=vp8" });
+        var blob = new Blob(chunks, { type: "video/webm" });
         chunks = [];
 
         const blobToBase64 = blob => {
