ce84b686e08f364d804ecfa33153154f9af1557d
Liam K <karllauy@gmail.com>
2021-11-11 15:55:27 +0800

    update login page

--- pages/user-profile.vue
+++ pages/user-profile.vue
@@ -153,7 +153,7 @@ export default {
       this.$router.push("/swipe");
     },
     onClickLogout() {
-      this.$router.push("/login");
+      this.$router.push("/");
       //  TODO: call action that cleans user data & form data
       this.$store.dispatch("clearOnboardingFormStates");
     }
