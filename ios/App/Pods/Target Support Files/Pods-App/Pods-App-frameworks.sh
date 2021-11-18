6ea1b3a17e54528cded95fda038cfa889ca3e3d1
jkscheung <jkscheung@gmail.com>
2021-11-18 15:41:25 +0800

    Add capacitor ios files

--- ios/App/Pods/Target Support Files/Pods-App/Pods-App-frameworks.sh
+++ ios/App/Pods/Target Support Files/Pods-App/Pods-App-frameworks.sh
@@ -177,11 +177,17 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Capacitor/Capacitor.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CapacitorCamera/CapacitorCamera.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CapacitorCordova/Cordova.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CapacitorFilesystem/CapacitorFilesystem.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CordovaPlugins/CordovaPlugins.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Capacitor/Capacitor.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CapacitorCamera/CapacitorCamera.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CapacitorCordova/Cordova.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CapacitorFilesystem/CapacitorFilesystem.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CordovaPlugins/CordovaPlugins.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
