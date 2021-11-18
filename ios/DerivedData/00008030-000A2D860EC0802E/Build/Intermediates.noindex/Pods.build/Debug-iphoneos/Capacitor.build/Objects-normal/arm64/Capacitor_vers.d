3bb2b656f1d6f01af67a53a109756faddea1eaf9
Liam K <karllauy@gmail.com>
2021-11-18 16:41:33 +0800

    added ios dir to gitignore & removed ios dir

--- ios/DerivedData/00008030-000A2D860EC0802E/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Capacitor.build/Objects-normal/arm64/Capacitor_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jkscheung/zx/mollie/src/deepAR-vue/ios/DerivedData/00008030-000A2D860EC0802E/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Capacitor.build/DerivedSources/Capacitor_vers.c
