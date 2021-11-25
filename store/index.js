788d5c04194a08a520f17e0918f94c354a78f79d
Liam K <karllauy@gmail.com>
2021-11-25 13:24:36 +0800

    remove long console log message

--- store/index.js
+++ store/index.js
@@ -118,7 +118,7 @@ export const mutations = {
   },
   setPhoto(state, val) {
     state.photo = val;
-    console.log("VUEX: set photo to", state.photo);
+    //console.log("VUEX: set photo to", state.photo);
   },
   addPhotoToUserProfileMedia(state, index) {
     console.log("type of index is", typeof index, index);
@@ -174,7 +174,7 @@ export const mutations = {
   },
   setVideo(state, val) {
     state.video = val;
-    console.log("VUEX: set video to", state.video);
+    // console.log("VUEX: set video to", state.video);
   },
   setActiveEffectIcon(state, val) {
     state.activeEffectIcon = val;
